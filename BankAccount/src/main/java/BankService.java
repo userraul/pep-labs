4b18632943a24fa28423dd58c7cac38199629a5a
userraul <rangel.raul710@gmail.com>
2022-12-29 17:48:44 +0000

    Saved progress on BankAccount 2022-12-29 17:48:44 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        this.balance += amount;
     }
 
     /**
@@ -32,7 +32,8 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if (amount <= this.balance)
+            this.balance -= amount;
     }
 
     /**
@@ -40,6 +41,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return this.balance;
     }
 }
