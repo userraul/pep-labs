8e7138ea93745af84f25f62207bc60127d07002f
userraul <rangel.raul710@gmail.com>
2022-12-29 17:43:02 +0000

    Saved progress on GetMax 2022-12-29 17:43:02 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,6 +7,9 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max = -100000;
+        for(int i : arr)
+            max = i > max ? i : max;
+        return max;
     }
 }
